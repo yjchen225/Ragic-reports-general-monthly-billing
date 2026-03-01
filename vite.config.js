2b8f841a11370f1876f76641e20a57ae0dbf651a
yjchen225 <yjchen225@gmail.com>
2026-03-01 18:32:53 +0800

    fix wrong base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Ragic-reports-general-monthly-billing/", // ← 改成你的 repo 名稱（大小寫要一致）
   plugins: [react()],
 })
